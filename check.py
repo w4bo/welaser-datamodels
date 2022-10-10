905161cf5b628fe38968fc1264c507a4a2be1d1f
w4bo <you@example.com>
2022-10-10 12:11:24 +0000

    add test agrifarm

--- check.py
+++ check.py
@@ -1,6 +1,7 @@
 import json
 import os
 import geojson
+import re
 
 for root, dirs, files in os.walk("."):
     for file in sorted(filter(lambda f: f.endswith('.json'), files)):
@@ -12,6 +13,9 @@ for root, dirs, files in os.walk("."):
                 # all entities should have a type and an id
                 assert "id" in entity
                 assert "type" in entity
+                regexp = re.compile(r'\(|\)')
+                for key, value in entity.items():
+                    assert not regexp.search(str(value)), {"id": entity["id"], key: value}
                 if "Task" in path:
                     assert "actualLocation" in entity
                 if "location" in entity:
